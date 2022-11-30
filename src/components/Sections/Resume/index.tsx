eb70681c414f834407c31adad721e8e2d5098ca5
Xiangbo Gao (Barry) <XiangboGaoBarry@gmail.com>
2022-11-30 10:37:57 -0800

    fix bugs for deployment: unused imports

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -1,6 +1,6 @@
 import {FC, memo} from 'react';
 
-import {education, experience, SectionId, skills, employment, competitions} from '../../../data/data';
+import {education, experience, SectionId, employment, competitions} from '../../../data/data';
 import Section from '../../Layout/Section';
 import ResumeSection from './ResumeSection';
 // import {SkillGroup} from './Skills';
